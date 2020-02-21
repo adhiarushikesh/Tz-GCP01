9379a9adf614a2ca81bfd3dc68c8776b9002b715
mywebappRA002\adhia002 <adhia002@Outlook.com>
2020-02-21 15:41:01 +0800

    Update2

--- Tz-GCP01/Variable.tf
+++ Tz-GCP01/Variable.tf
@@ -5,7 +5,8 @@ variable "gcp_project" {
     default = "Tz-test"
 }
 variable "credentials" {
-    default = ""
+    default = "credential.json"
+    
 }
 variable "name" {}
 variable "subnet_cidr" {}
--- Tz-GCP01/main.tf
+++ Tz-GCP01/main.tf
@@ -1,11 +1,12 @@
 provider "google" {
   version = "3.5.0"
 
-  credentials = file("tz-test-268804-4b500dc542d5.json")
+  credentials = "${file("${var.credentials}")}"
 
-  project = "Tz-test"
-  region  = "asia-southeast1"
-  zone    = "a, b, c"}
+  project = "${var.gcp_project}"
+  region  = "${var.region}"
+  zone    = "a, b, c"
+  }
 
 resource "google_compute_network" "vpc_network" {
   name = "terraform-network"
